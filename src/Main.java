4172473424dc719224829c16835d7742cbbf1c9d
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-10-04 14:25:17 -0600

    moneyTransfer

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 2000_000_000;
+        int transfer = 500_000_000;
+        int finbal = balance+transfer;
+
+        System.out.println(finbal);
+    }
+}
